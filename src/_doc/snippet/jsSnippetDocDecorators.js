c37fcbdcf9c722a97310acc95866c29a1f53b647
Ben Verbist <ben.verbist@kunstmaan.be>
2016-11-27 00:03:18 +0100

    [Ben] added and used the builder decorator snippet

--- src/_doc/snippet/jsSnippetDocDecorators.js
+++ src/_doc/snippet/jsSnippetDocDecorators.js
@@ -1,6 +1,7 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 
 import inspectJsSnippet from './inspectJsSnippet';
+import builder from '../../decorator/builder';
 import buildIfBuilder from '../../builder/buildIfBuilder';
 import {is} from '../../index';
 
@@ -8,7 +9,7 @@ export const snippet = (jsSnippet) =>
     function decorate(target) {
         const {docType, name} = inspectJsSnippet(jsSnippet);
 
-        target.doc = {
+        const initial = {
             code: jsSnippet,
             type: docType,
             name: is.set(name) ? name : getNameOfClass(target),
@@ -19,57 +20,49 @@ export const snippet = (jsSnippet) =>
             authors: []
         };
 
-        target.build = () => target.doc;
+        builder(initial)(target);
 
         return target;
     };
 
 export const type = (val) =>
     function decorate(target) {
-        target.doc.type = val;
-        return target;
+        return target.with('type', val);
     };
 
 export const desc = (description) =>
     function decorate(target) {
-        target.doc.desc = description;
-        return target;
+        return target.with('desc', description);
     };
 
 export const params = (...paramJsPropBuilders) =>
     function decorate(target) {
-        target.doc.params = buildIfBuilder(paramJsPropBuilders);
-        return target;
+        return target.with('params', buildIfBuilder(paramJsPropBuilders));
     };
 
 export const returns = (returnsJsPropBuilder) =>
     function decorate(target) {
-        target.doc.returns = buildIfBuilder(returnsJsPropBuilder);
-        return target;
+        return target.with('returns', buildIfBuilder(returnsJsPropBuilder));
     };
 
 export const throws = (...potentialErrors) =>
     function decorate(target) {
-        target.doc.throws = potentialErrors;
-        return target;
+        return target.with('throws', potentialErrors);
     };
 
 export const examples = (...exampleFuncs) =>
     function decorate(target) {
-        target.doc.examples = exampleFuncs;
-        return target;
+        return target.with('examples', exampleFuncs);
     };
 
 export const parts = (...partSnippetDocs) =>
     function decorate(target) {
-        target.doc.parts = buildIfBuilder(partSnippetDocs);
-        return target;
+        return target.with('parts', buildIfBuilder(partSnippetDocs));
     };
 
 export const authors = (...authorNames) =>
     function decorate(target) {
-        target.doc.authors = authorNames;
-        return target;
+        return target.with('authors', authorNames);
     };
 
 function getNameOfClass(clazz) {
---
+++ src/decorator/builder.js
@@ -0,0 +1,23 @@
+import {is} from '../index';
+
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
+const builder = (initialBuildParams = {}, buildStateName = '_builder') =>
+    function decorate(target) {
+        if (is.undefined(target[buildStateName])) {
+            target[buildStateName] = initialBuildParams;
+        } else {
+            Object.assign(target[buildStateName], initialBuildParams);
+        }
+
+        target.with = (key, val) => {
+            target[buildStateName][key] = val;
+            return target;
+        };
+
+        target.build = () => target[buildStateName];
+
+        return target;
+    };
+
+export default builder;
