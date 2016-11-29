1fb8d82c023f993c5db7dc8a053a2ab093e41810
Ben Verbist <ben.verbist@kunstmaan.be>
2016-11-29 17:02:33 +0100

    [Ben] added doc for the assert snippet

--- src/_docs.js
+++ src/_docs.js
@@ -1,10 +1,12 @@
 import {group, registerGroups} from './_docRef';
 
+import assertDoc from './generic/assert.doc';
 import isDoc from './generic/is.doc';
 import collapsableDoc from './decorator/collapsable.doc';
 
 registerGroups([
     group('generic').snippets(
+        assertDoc,
         isDoc
     ),
     group('decorator').snippets(
