b7ae0e226e34f38049dc014530e910488c5a1b95
Ben Verbist <ben.verbist@kunstmaan.be>
2017-05-11 13:49:34 +0200

    [Ben] start for showing the registered doc groups

--- components/user/userSelectors.js
+++ components/user/userSelectors.js
@@ -1,5 +1,14 @@
+import {getRegisteredGroups} from '../../src/_doc/documentJs';
+
 export const getLang = (state) =>
     state.user.lang;
 
 export const getVersion = (state) =>
     state.user.version;
+
+export const getDocGroupsForCurrentVersion = (state) => {
+    const version = getVersion(state);
+
+    // TODO other versions
+    return (version === 'latest') ? getRegisteredGroups() : [];
+}
