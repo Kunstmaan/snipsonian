310f3db7e21ac4ceaeb18a469913cb9ed68095c0
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-05-17 17:40:01 +0200

    v0.3.0

---
+++ prev_versions/0.3.0/_docRef.js
@@ -0,0 +1,9 @@
+/* eslint import/no-unresolved: ["off"] */
+export {group, registerGroups, getRegisteredGroups} from '../../docTreeGenerator/documentJs';
+
+export {snippet, type, name, desc, param, returns, canThrow, examples, parts, authors}
+    from '../../docTreeGenerator/snippet/jsSnippetDocDecorators';
+
+export {decorate} from './decorator/decorateWith';
+
+export {JS_DOC_TYPE} from '../../docTreeGenerator/snippet/jsDocTypes';
