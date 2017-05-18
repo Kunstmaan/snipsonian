45448827daa01598bdeef18955625713be885f74
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-05-18 11:29:02 +0200

    [Thomas] * 'Go to docs' link now points to the version defined in the store

--- components/header/Header.component.jsx
+++ components/header/Header.component.jsx
@@ -3,29 +3,67 @@ import {Link} from 'react-router';
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
 import {prefixLink} from 'gatsby-helpers';
 /* eslint-enable */
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import {getVersion} from '../user/userSelectors';
+import {switchVersion} from '../user/userActions';
 
 import styleConfig from '../../config/style.config';
 
-const Header = () => (
-    <div className="header-container">
-        <div>
-            <Link to={prefixLink('/')}
-                  style={{
-                      textDecoration: 'none',
-                      color: styleConfig.header.title.color
-                  }}>
-                <span className="brand">Snipsonian</span>
-            </Link>
-        </div>
-        <span style={{
-            color: styleConfig.color.primary
-        }}>
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="header-container">
+                <div>
+                    <Link to={prefixLink('/')}
+                          style={{
+                              textDecoration: 'none',
+                              color: styleConfig.header.title.color
+                          }}>
+                        <span className="brand">Snipsonian</span>
+                    </Link>
+                </div>
+                <span style={{
+                    color: styleConfig.color.primary
+                }}>
                     Small, re-usable javascript code snippets
                 </span>
-        <Link to={prefixLink('/doc/latest/')} style={{color: styleConfig.header.title.color}}>Go To Docs</Link>
-        <span>powered by Kunstmaan Development</span>
-        <Link to={prefixLink('/usage/')}>usage</Link>
-    </div>
-);
+                <Link to={prefixLink(`/doc/${this.props.version}/`)} style={{color: styleConfig.header.title.color}}>Go To Docs</Link>
+                <span>powered by Kunstmaan Development</span>
+            </div>
+        );
+    }
+}
 
-export default Header;
+Header.propTypes = {
+    version: PropTypes.string.isRequired
+};
+
+Header.defaultProps = {
+    version: 'latest'
+};
+
+function mapDispatchToProps(dispatch) {
+    return {
+        switchVersion: (selectedVersion) => {
+            dispatch(switchVersion(selectedVersion));
+        }
+    };
+}
+
+function mapStateToProps(state) {
+    return {
+        version: getVersion(state)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Header);
