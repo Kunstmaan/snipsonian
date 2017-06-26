c175fd5eea906d408885030292291d5559ae5af8
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 17:25:13 +0200

    [Automatic] pre-version commit

--- bin/copyLatestToVersion/config.js
+++ bin/copyLatestToVersion/config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 const packageJson = require('../../package.json');
 
-const DOC_TREE_GENERATOR_SRC = '../docTreeGenerator';
+const DOC_TREE_GENERATOR_SRC = '../../docTreeGenerator';
 const DOC_TREE_GENERATOR_DEST = '../../docTreeGenerator';
-const PAGES_PATH = path.resolve(__dirname, '../pages/doc');
-const SOURCE_DIR = path.resolve(__dirname, '../src');
+const PAGES_PATH = path.resolve(__dirname, '../../pages/doc');
+const SOURCE_DIR = path.resolve(__dirname, '../../src');
 const NEW_VERSION = packageJson.version;
-const PREV_VERSION_PATH = path.resolve(__dirname, '../prev_versions');
+const PREV_VERSION_PATH = path.resolve(__dirname, '../../prev_versions');
 const DEST_DIR = path.resolve(PREV_VERSION_PATH, NEW_VERSION);
 
 module.exports = {
