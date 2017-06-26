49dcd01f847f711629d7c7cdfddddce2094e87f3
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 17:28:29 +0200

    [Automatic] pre-version commit

--- bin/copyLatestToVersion/convertSrcPathToDestPath.js
+++ bin/copyLatestToVersion/convertSrcPathToDestPath.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const config = require('./config');
+
 module.exports = function convertSrcPathToDestPath(srcPath) {
-    return path.resolve(DEST_DIR, srcPath.split('src/')[1] || '');
+    return path.resolve(config.DEST_DIR, srcPath.split('src/')[1] || '');
 }
