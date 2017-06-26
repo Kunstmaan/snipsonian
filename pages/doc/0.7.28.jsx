7950c37a05bf2d5128e1ebe2ff11d0c216af78a9
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 12:09:00 +0200

    v0.7.28

--- pages/doc/0.7.28.jsx
+++ pages/doc/0.7.28.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Version from '../../components/version/Version';
-import docs from '../../prev_versions/0.7.23/_docs';
+import docs from '../../prev_versions/0.7.28/_docs';
 
 const versionConfig = {
     docs,
-    v: '0.7.23'
+    v: '0.7.28'
 };
 
 export default () => (
--- prev_versions/0.7.21/node/writeFile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function writeFile({filePath, data, options = {}, fs}) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filePath, data, options, (err) => {
-            if (err) return reject(err);
-            return resolve();
-        });
-    });
-};
