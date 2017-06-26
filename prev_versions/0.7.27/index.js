8a305b3236e48d18d98340410d9647bd681b7dc9
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 11:24:44 +0200

    v0.7.27

---
+++ prev_versions/0.7.27/index.js
@@ -0,0 +1,7 @@
+/**
+ * Only exports generic snippets.
+ * This is to prevent that you get/bundle all snippets if you just need a few.
+ */
+
+export {is} from './generic/is';
+export {assert} from './generic/assert';
