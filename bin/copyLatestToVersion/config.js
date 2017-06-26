4ca08b4e3d94c7ae70ae05d2575d9fb0c8c0246e
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 17:16:27 +0200

    [Automatic] pre-version commit

--- bin/copyLatestToVersion/config.js
+++ bin/copyLatestToVersion/config.js
@@ -1,12 +1,21 @@
 const path = require('path');
-const packageJson = require('../../package.json');
+
+const packageJson = require('./package.json');
+
+const DOC_TREE_GENERATOR_SRC = '../docTreeGenerator';
+const DOC_TREE_GENERATOR_DEST = '../../docTreeGenerator';
+const PAGES_PATH = path.resolve(__dirname, '../pages/doc');
+const SOURCE_DIR = path.resolve(__dirname, '../src');
+const NEW_VERSION = packageJson.version;
+const PREV_VERSION_PATH = path.resolve(__dirname, '../prev_versions');
+const DEST_DIR = path.resolve(PREV_VERSION_PATH, NEW_VERSION);
 
 module.exports = {
-    DOC_TREE_GENERATOR_SRC: '../docTreeGenerator',
-    DOC_TREE_GENERATOR_DEST: '../../docTreeGenerator',
-    PAGES_PATH: path.resolve(__dirname, '../pages/doc'),
-    SOURCE_DIR: path.resolve(__dirname, '../src'),
-    NEW_VERSION: packageJson.version,
-    PREV_VERSION_PATH: path.resolve(__dirname, '../prev_versions'),
-    DEST_DIR: path.resolve(this.PREV_VERSION_PATH, this.NEW_VERSION)
+    DOC_TREE_GENERATOR_SRC,
+    DOC_TREE_GENERATOR_DEST,
+    PAGES_PATH,
+    SOURCE_DIR,
+    NEW_VERSION,
+    PREV_VERSION_PATH,
+    DEST_DIR
 };
