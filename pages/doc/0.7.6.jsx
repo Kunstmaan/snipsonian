7b43747e83c5983aa952220ae6719407d54648f5
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-26 09:46:51 +0200

    v0.7.6

--- pages/doc/0.7.6.jsx
+++ pages/doc/0.7.6.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Version from '../../components/version/Version';
-import docs from '../../prev_versions/0.7.5/_docs';
+import docs from '../../prev_versions/0.7.6/_docs';
 
 const versionConfig = {
     docs,
-    v: '0.7.5'
+    v: '0.7.6'
 };
 
 export default () => (
