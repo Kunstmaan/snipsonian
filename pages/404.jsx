ef378cd4bac4c18c37dd3e2a02501d626525ac99
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-27 16:56:04 +0200

    [Thomas] Added a button to the 404 that will take you to the correct patch version if it's not there.

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router';
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+import {prefixLink} from 'gatsby-helpers';
+/* eslint-enable */
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
-const page404 = () => (
+import {getVersion} from '../components/user/userSelectors';
+
+const page404 = ({version}) => (
     <div className="page404">
         <h1>NOT FOUND</h1>
         <p>{'You just hit a route that doesn\'t exist... the sadness.'}</p>
+        <h3>But Wait! Not all is lost yet!</h3>
+        <p>
+            {`You might have got an outdated link there. We probably deleted the exact version you had in your url.
+            This happens a lot, soooo... we created a button for that. When you click on it, you'll be taken to the version with the same major and minor version, but a different patch.`}
+        </p>
+        <Link to={prefixLink(`/doc/${version}/${window.location.hash}`)}><button>To Correct Version</button></Link>
     </div>
 );
 
-export default page404;
+function mapStateToProps(state) {
+    return {
+        version: getVersion(state)
+    };
+}
+
+page404.propTypes = {
+    version: PropTypes.string.isRequired
+};
+
+page404.defaultProps = {
+    version: 'latest'
+};
+
+export default connect(
+    mapStateToProps
+)(page404);
