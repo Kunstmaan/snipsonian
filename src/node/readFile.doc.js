181fd405952e9138a82fd9b010e74ef83cdeb573
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-06-27 12:46:49 +0200

    [Automatic] pre-version commit

--- src/node/readFile.doc.js
+++ src/node/readFile.doc.js
@@ -16,7 +16,7 @@ import {snippet, name, desc, authors, signature, since, param, JS_DOC_TYPE} from
     isOptional: true
 })
 @authors('Thomas Seberechts')
-@since('<$SINCE$>')
+@since('0.8.1')
 @signature('({filePath, options = {})')
 @snippet(readFile)
 class readFileDoc {
