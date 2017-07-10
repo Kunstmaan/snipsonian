509433e6fe8b84f219a6842c0931f1a07d6581ec
Thomas Seberechts <thomas.seberechts@kunstmaan.be>
2017-07-10 10:03:55 +0200

    [Thomas] exclude files in 'src' folder from snippets array

--- bin/verifyDocumentation/splitFilesInSnippetAndDoc.js
+++ bin/verifyDocumentation/splitFilesInSnippetAndDoc.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function splitFilesInSnippetAndDoc(data) {
     return new Promise((resolve) => {
         const result = {
@@ -7,7 +9,7 @@ module.exports = function splitFilesInSnippetAndDoc(data) {
         data.forEach((file) => {
             if (file.includes('.doc.')) {
                 result.docs.push(file);
-            } else {
+            } else if (path.dirname(file).split(path.sep).pop() !== 'src') {
                 result.snippets.push(file);
             }
         });
