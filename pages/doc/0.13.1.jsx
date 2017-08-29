35efdbf9f86bdf3dadea5642068600a7b57931f7
Ben Verbist <ben.verbist@kunstmaan.be>
2017-08-29 16:30:43 +0200

    v0.13.1

--- pages/doc/0.13.1.jsx
+++ pages/doc/0.13.1.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Version from '../../components/version/Version';
-import docs from '../../prev_versions/0.13.0/_docs';
+import docs from '../../prev_versions/0.13.1/_docs';
 
 const versionConfig = {
     docs,
-    v: '0.13.0'
+    v: '0.13.1'
 };
 
 export default () => (
