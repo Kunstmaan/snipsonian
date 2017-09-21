b5e7b272bb289a1eec06905183c1fdd8e8664373
Ben Verbist <ben.verbist@kunstmaan.be>
2017-09-21 12:15:38 +0200

    v0.14.0

---
+++ prev_versions/0.14.0/analytics/tracker.js
@@ -0,0 +1,97 @@
+/* global ga */
+
+/**
+ * The optional custom object param can be used for custom dimensions and metrics
+ * See https://developers.google.com/analytics/devguides/collection/analyticsjs/custom-dims-mets
+ */
+
+let onSendVirtualPageview = ({virtualUrl, custom}) => {
+    ga('set', 'page', virtualUrl);
+    ga('send', 'pageview', custom);
+};
+
+let onSendEvent = ({category, action, label, value, isNonInteraction, custom}) => {
+    ga('send', Object.assign({
+        hitType: 'event',
+        eventCategory: category,
+        eventAction: action,
+        eventLabel: label,
+        eventValue: value,
+        nonInteraction: isNonInteraction
+    }, custom));
+};
+
+let onSendSocial = ({network, action, target, custom}) => {
+    ga('send', Object.assign({
+        hitType: 'social',
+        socialNetwork: network,
+        socialAction: action,
+        socialTarget: target
+    }, custom));
+};
+
+let onSendTiming = ({category, timingVar, value, label, custom}) => {
+    ga('send', Object.assign({
+        hitType: 'timing',
+        timingCategory: category,
+        timingVar,
+        timingValue: value,
+        timingLabel: label
+    }, custom));
+};
+
+let onSendException = ({description, isFatal, custom}) => {
+    ga('send', Object.assign({
+        hitType: 'exception',
+        exDescription: description,
+        exFatal: isFatal
+    }, custom));
+};
+
+export function overrideTracker({
+    newOnSendVirtualPageview,
+    newOnSendEvent,
+    newOnSendSocial,
+    newOnSendTiming,
+    newOnSendException
+} = {}) {
+    if (newOnSendVirtualPageview) {
+        onSendVirtualPageview = newOnSendVirtualPageview;
+    }
+
+    if (newOnSendEvent) {
+        onSendEvent = newOnSendEvent;
+    }
+
+    if (newOnSendSocial) {
+        onSendSocial = newOnSendSocial;
+    }
+
+    if (newOnSendTiming) {
+        onSendTiming = newOnSendTiming;
+    }
+
+    if (onSendException) {
+        onSendException = newOnSendException;
+    }
+}
+
+export function sendVirtualPageview({virtualUrl, custom = {}}) {
+    onSendVirtualPageview({virtualUrl, custom});
+}
+
+export function sendEvent({category, action, label, value, isNonInteraction = false, custom = {}}) {
+    onSendEvent({category, action, label, value, isNonInteraction, custom});
+}
+
+export function sendSocial({network, action, target, custom = {}}) {
+    onSendSocial({network, action, target, custom});
+}
+
+export function sendTiming({category, timingVar, value, label, custom = {}}) {
+    onSendTiming({category, timingVar, value, label, custom});
+}
+
+export function sendException({description, isFatal = false, custom = {}}) {
+    onSendException({description, isFatal, custom});
+}
