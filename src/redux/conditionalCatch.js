784fd95fdbf32299d589fa07d838434a56afdd78
Jan Bevers <jan.bevers@kunstmaan.be>
2017-09-29 12:09:11 +0200

    Fixes to support checkJs option in Typescript

--- src/redux/conditionalCatch.js
+++ src/redux/conditionalCatch.js
@@ -1,11 +1,11 @@
 const NOOP = () => {};
 
-export default function conditionalCatch({shouldCatchErrors = false, actionToExecute, onError = NOOP}) {
+export default function conditionalCatch({shouldCatchErrors = false, actionToExecute, onError}) {
     if (shouldCatchErrors) {
         try {
             return actionToExecute();
         } catch (error) {
-            return onError(error);
+            return onError ? onError(error) : NOOP;
         }
     } else {
         return actionToExecute();
