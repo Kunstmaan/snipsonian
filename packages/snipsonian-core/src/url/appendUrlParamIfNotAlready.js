26af5446a7af690845e41fbc4f5f9213f68050f0
Benno Meysmans <benno.meysmans@kunstmaan.be>
2018-04-03 11:14:23 +0200

    Fix appenUrlParamIfNotAlready and add tests

--- packages/snipsonian-core/src/url/appendUrlParamIfNotAlready.js
+++ packages/snipsonian-core/src/url/appendUrlParamIfNotAlready.js
@@ -9,5 +9,6 @@ export default function appendUrlParamIfNotAlready({ url, paramName, paramValue 
 }
 
 function doesUrlContainParam({ url, paramName }) {
-    return url.search(new RegExp(`[\\?&]${paramName}=`)) > -1;
+    const encodedParamName = encodeURIComponent(paramName);
+    return url.search(new RegExp(`[\\?&]${encodedParamName}=`)) > -1;
 }
