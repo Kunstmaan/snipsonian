3282474054a99327af9659ffff6488a2107bc15f
Ben Verbist <ben.verbist@kunstmaan.be>
2019-03-26 10:04:32 +0100

    to-typescript: script to replace the /src/ imports to /es/ imports in the transpiled code

---
+++ bin/code/replaceSnipsonianImports.js
@@ -0,0 +1,20 @@
+const replace = require('replace-in-file');
+
+replaceSnipsonianSrcImportsToEsImports();
+
+async function replaceSnipsonianSrcImportsToEsImports() {
+    console.error('Replacing snipsonian-src-imports with es-imports ...');
+
+    const replaceOptions = {
+        files: 'packages/*/es/**/*.js',
+        from: /'@snipsonian\/[a-zA-Z\-]*\/src\//g,
+        to: (match) => match.replace('/src/', '/es/'),
+    };
+
+    try {
+        const changes = await replace(replaceOptions)
+        console.log('Modified files:', changes.join(', '));
+    } catch (error) {
+        console.error('Error occurred:', error);
+    }
+}
