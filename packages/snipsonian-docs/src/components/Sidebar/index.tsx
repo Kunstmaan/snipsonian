4075d0b1e5f80c4c69e55c807845b4350cfb7aef
vdwjeff <vdwjeff@gmail.com>
2020-01-24 16:43:32 +0100

    Display active package in side bar nav

--- packages/snipsonian-docs/src/components/Sidebar/index.tsx
+++ packages/snipsonian-docs/src/components/Sidebar/index.tsx
@@ -16,56 +16,20 @@ function Sidebar({ navigation }: IPublicProps) {
                 {navigation.map((item) => (
                     <NavItem key={item.to} to={item.to} name={item.title} />
                 ))}
-                {/* {Object.keys(docs).map((folderName) => {
-                    const doc = docs[folderName];
-                    const level = 1;
-                    return (
-                        <Folder key={folderName} title={folderName}>
-                            <Children level={level} items={doc.children} />
-                        </Folder>
-                    );
-                })} */}
             </nav>
         </aside>
     );
 }
 
 function NavItem({ to, name }: { to: string; name: string }) {
+    const { pathname } = window.location;
+    const isActive = pathname && pathname.startsWith(`/snipsonian-${name}/`);
     return (
-        <div className={`${CLASS_NAME}__nav-item`}>
+        <div className={`${CLASS_NAME}__nav-item ${isActive && 'active'}`}>
             <Link to={to} >{name}</Link>
         </div>
     );
 }
-
-// function renderFolder({ item, level }: { item: IDocumentationItem; level: number }) {
-//     const { name, slug, type, children } = item;
-
-//     if (type === 'file') {
-//         return (
-//             <ItemLink key={slug} to={slug} name={name} />
-//         );
-//     }
-//     if (type === 'folder') {
-//         const childLevel = level + 1;
-//         return (
-//             <Folder key={slug} title={name}>
-//                 <Children level={childLevel} items={children} />
-//             </Folder>
-
-//         );
-//     }
-//     return null;
-// }
-
-// function Folder({ children, title }: { children: React.ReactNode; title: string }) {
-//     return (
-//         <div className={`${CLASS_NAME}__folder`}>
-//             <h4>{title}</h4>
-//             {children}
-//         </div>
-//     );
-// }
 
 export default Sidebar;
 
