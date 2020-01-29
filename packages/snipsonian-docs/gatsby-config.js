1e444b6ea9dc083517a231508427f631e2b0cba2
vdwjeff <vdwjeff@gmail.com>
2020-01-29 10:23:28 +0100

    Added PurgeCss plugin and removed unused plugins

--- packages/snipsonian-docs/gatsby-config.js
+++ packages/snipsonian-docs/gatsby-config.js
@@ -8,18 +8,20 @@ module.exports = {
         'gatsby-plugin-postcss',
         'gatsby-plugin-typescript',
         'gatsby-plugin-react-helmet',
-        // {
-        //     resolve: 'gatsby-source-filesystem',
-        //     options: {
-        //         name: 'docs',
-        //         path: `${__dirname}/docs/`,
-        //     },
-        // },
-        'gatsby-transformer-json',
-        'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
+        {
+            resolve: 'gatsby-plugin-purgecss',
+            options: {
+                printRejected: true, // Print removed selectors and processed file names
+                // develop: true, // Enable while using `gatsby develop`
+                tailwind: true, // Enable tailwindcss support
+                // whitelist: ['whitelist'], // Don't remove this selector
+                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+                // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+            },
+        },
     ],
 };
